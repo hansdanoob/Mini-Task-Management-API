ce5ed792395d086cf5a1910543507f01b8dfaa5f
hansdanoob <josiahandrews9@gmail.com>
2025-12-30 14:35:31 -0700

    Task Class + Status enum

---
+++ task-api/src/main/java/com/task/api/task_api/Status.java
@@ -0,0 +1,7 @@
+package main.java.com.task.api.task_api;
+
+public enum Status {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
---
+++ task-api/src/main/java/com/task/api/task_api/Task.java
@@ -0,0 +1,80 @@
+package main.java.com.task.api.task_api;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // automatically generates id when new Task object is created
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+
+    private Status status = Status.TODO; // enum storing: TODO - IN_PROGRESS - DONE
+
+
+    // Constructors
+    public Task() {
+    }
+
+    public Task(String title, String description) {
+        Task(title, description, "TODO");   // default status is "TODO"
+    }
+
+    public Task(String title, String description, String status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+    // Getters/Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) { // not necessary?
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) { // not necessary?
+        this.createdAt = createdAt;
+    }
+}
